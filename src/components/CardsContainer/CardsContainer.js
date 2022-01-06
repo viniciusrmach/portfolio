91ba0b6234fe269306c4fce61ce3edbeeca0fd58
Gabriel Valverde <gabriel_gom@hotmail.com>
2022-01-06 00:26:02 -0300

    Change: Separate file for the "works" array

--- src/components/CardsContainer/CardsContainer.js
+++ src/components/CardsContainer/CardsContainer.js
@@ -1,46 +1,7 @@
 import styles from './CardsContainer.module.scss';
-
-import ImageRioShowGastronomia from './images/img-exemple.png';
+import works from './data/works';
 
 import Card from './Card/Card';
-
-const works = [
-  {
-    image: ImageRioShowGastronomia,
-    title: 'RioShow Gastronomia',
-    description:
-      'Os melhores restaurantes eleitos pela crítica reunidos em um guia',
-    client: 'RioShow • 2019',
-  },
-  {
-    image: ImageRioShowGastronomia,
-    title: 'RioShow Gastronomia 2',
-    description:
-      'Os melhores restaurantes eleitos pela crítica reunidos em um guia',
-    client: 'RioShow • 2019',
-  },
-  {
-    image: ImageRioShowGastronomia,
-    title: 'RioShow Gastronomia 3',
-    description:
-      'Os melhores restaurantes eleitos pela crítica reunidos em um guia',
-    client: 'RioShow • 2019',
-  },
-  {
-    image: ImageRioShowGastronomia,
-    title: 'RioShow Gastronomia 4',
-    description:
-      'Os melhores restaurantes eleitos pela crítica reunidos em um guia',
-    client: 'RioShow • 2019',
-  },
-  {
-    image: ImageRioShowGastronomia,
-    title: 'RioShow Gastronomia 5',
-    description:
-      'Os melhores restaurantes eleitos pela crítica reunidos em um guia',
-    client: 'RioShow • 2019',
-  },
-];
 
 function CardsContainer() {
   return (
---
+++ src/components/CardsContainer/data/works.js
@@ -0,0 +1,41 @@
+import ImageRioShowGastronomia from '../images/img-exemple.png';
+
+const works = [
+  {
+    image: ImageRioShowGastronomia,
+    title: 'RioShow Gastronomia',
+    description:
+      'Os melhores restaurantes eleitos pela crítica reunidos em um guia',
+    client: 'RioShow • 2019',
+  },
+  {
+    image: ImageRioShowGastronomia,
+    title: 'RioShow Gastronomia 2',
+    description:
+      'Os melhores restaurantes eleitos pela crítica reunidos em um guia',
+    client: 'RioShow • 2019',
+  },
+  {
+    image: ImageRioShowGastronomia,
+    title: 'RioShow Gastronomia 3',
+    description:
+      'Os melhores restaurantes eleitos pela crítica reunidos em um guia',
+    client: 'RioShow • 2019',
+  },
+  {
+    image: ImageRioShowGastronomia,
+    title: 'RioShow Gastronomia 4',
+    description:
+      'Os melhores restaurantes eleitos pela crítica reunidos em um guia',
+    client: 'RioShow • 2019',
+  },
+  {
+    image: ImageRioShowGastronomia,
+    title: 'RioShow Gastronomia 5',
+    description:
+      'Os melhores restaurantes eleitos pela crítica reunidos em um guia',
+    client: 'RioShow • 2019',
+  },
+];
+
+export default works;
