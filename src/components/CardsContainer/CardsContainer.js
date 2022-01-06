8518e75407cac5dc5365a265145a4493684f73dd
Gabriel Valverde <gabriel_gom@hotmail.com>
2022-01-06 00:44:21 -0300

    Add: CSS Grid on CardsContainer

--- src/components/CardsContainer/CardsContainer.js
+++ src/components/CardsContainer/CardsContainer.js
@@ -5,15 +5,17 @@ import Card from './Card/Card';
 
 function CardsContainer() {
   return (
-    <div className={styles.container}>
-      {works.map((work) => (
-        <Card
-          image={work.image}
-          title={work.title}
-          description={work.description}
-          client={work.client}
-        />
-      ))}
+    <div className={styles['outer-container']}>
+      <div className={styles['inner-container']}>
+        {works.map((work) => (
+          <Card
+            image={work.image}
+            title={work.title}
+            description={work.description}
+            client={work.client}
+          />
+        ))}
+      </div>
     </div>
   );
 }
