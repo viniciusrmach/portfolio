66c5357b410b82b4c9024aa4b228efbe13d38851
Gabriel Valverde <gabriel_gom@hotmail.com>
2022-04-25 00:24:24 +0100

    Fix: Improvements on Navbar CSS

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,53 +12,55 @@ function Navbar() {
       <h1 className={styles.logo}>
         <Link to="/">vinicius machado</Link>
       </h1>
+
       <div className={styles['menu-icon']}>
         <div className={styles['menu-line']}></div>
         <div className={styles['menu-line']}></div>
         <div className={styles['menu-line']}></div>
       </div>
 
-      <nav className={styles.menu}>
-        <ul>
-          <li className={styles.item}>
-            <Link to="/">
-              projetos <p>/ work</p>
-            </Link>
-          </li>
-          <li className={styles.item2}>
-            <Link to="what-i-do">
-              o que eu faço <p>/ what I do</p>
-            </Link>
-          </li>
-          <li className={styles.item3}>
-            <Link to="about-me">
-              sobre mim <p>/ about me</p>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <div className={styles['menu-desk']}>
+        <nav className={styles.menu}>
+          <ul>
+            <li>
+              <Link to="/">
+                projetos <span>/ work</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="what-i-do">
+                o que eu faço <span>/ what I do</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="about-me">
+                sobre mim <span>/ about me</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
 
-      <div className={styles.social}>
-        <img className={styles['ico-email']} src={imageEmail} />
-
-        <img
-          className={styles['ico-linkedin']}
-          src={imageLinkedin}
-          alt={'LinkedIn'}
-        />
-
-        <img
-          className={styles['ico-twitter']}
-          src={imageTwitter}
-          alt={'Twitter'}
-        />
-
-        <img
-          className={styles['ico-instagram']}
-          src={imageInstagram}
-          alt={'Instagram'}
-        />
+        <div className={styles.social}>
+          <img className={styles['ico-email']} src={imageEmail} />
+          <img
+            className={styles['ico-linkedin']}
+            src={imageLinkedin}
+            alt={'LinkedIn'}
+          />
+          <img
+            className={styles['ico-twitter']}
+            src={imageTwitter}
+            alt={'Twitter'}
+          />
+          <img
+            className={styles['ico-instagram']}
+            src={imageInstagram}
+            alt={'Instagram'}
+          />
+        </div>
       </div>
+
+      <div className={styles['menu-mobi']}>mobi menu</div>
     </header>
   );
 }
