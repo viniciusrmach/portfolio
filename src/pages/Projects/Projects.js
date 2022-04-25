51ea1ace7f052b78a930d3e6966c8fad47513f1e
Gabriel Valverde <gabriel_gom@hotmail.com>
2022-04-25 22:52:16 +0100

    Add: Projects page name on mobi

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,8 +1,12 @@
+import styles from './Projects.module.scss';
 import CardsContainer from '../../components/CardsContainer/CardsContainer';
 
 function Projects() {
   return (
     <>
+      <h2 className={styles['page-name']}>
+        projetos <span>/ work</span>
+      </h2>
       <CardsContainer />
     </>
   );
