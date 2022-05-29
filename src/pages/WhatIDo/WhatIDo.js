c63af64e82ea4807d49ef952864cf95aba567bba
Gabriel Valverde <gabriel_gom@hotmail.com>
2022-05-29 20:23:28 +0100

    Add: WhatIDo page

--- src/pages/WhatIDo/WhatIDo.js
+++ src/pages/WhatIDo/WhatIDo.js
@@ -1,8 +1,53 @@
+import styles from './WhatIDo.module.scss';
+
+import placeholderImage from './images/540x332.png';
+
 function WhatIDo() {
   return (
-    <>
-      <div>What I Do</div>
-    </>
+    <div className={styles['main-container']}>
+      <div className={styles['mobi-padding']}>
+        <h2 className={styles['title-primary']}>
+          interface + design de informação
+        </h2>
+        <p className={styles['text-primary']}>
+          Em até 6 linhas você precisa contar: onde você vive, onde estudou,
+          experiências de trabalho e cursos que fez, área de atuação, projetos
+          pessoais que fez, interesses e dizer que é sócio-fundador da Nautilus.
+          Em até 10 linhas você precisa contar: onde você vive, onde estudou.
+        </p>
+
+        <h3 className={styles['title-secondary']}>interface</h3>
+        <p className={styles['text-primary']}>
+          Em até 6 linhas você precisa contar: onde você vive, onde estudou,
+          experiências de trabalho e cursos que fez, área de atuação, projetos
+          pessoais que fez, interesses e dizer que é sócio-fundador da Nautilus.
+          Em até 10 linhas você precisa contar: onde você vive, onde estudou.
+        </p>
+
+        <img className={styles['content-image']} src={placeholderImage} />
+
+        <p className={styles['text-primary']}>
+          Em até 6 linhas você precisa contar: onde você vive, onde estudou,
+          experiências de trabalho e cursos que fez, área de atuação, projetos
+          pessoais que fez, interesses e dizer que é sócio-fundador da Nautilus.
+          Em até 10 linhas você precisa contar: onde você vive, onde estudou.
+        </p>
+
+        <img className={styles['content-image']} src={placeholderImage} />
+
+        <h3 className={styles['title-secondary']}>Design da informação</h3>
+        <p className={styles['text-primary']}>
+          Em até 6 linhas você precisa contar: onde você vive, onde estudou,
+          experiências de trabalho e cursos que fez, área de atuação, projetos
+          pessoais que fez, interesses e dizer que é sócio-fundador da Nautilus.
+          Em até 10 linhas você precisa contar: onde você vive, onde estudou.
+        </p>
+
+        <img className={styles['content-image']} src={placeholderImage} />
+
+        <img className={styles['content-image']} src={placeholderImage} />
+      </div>
+    </div>
   );
 }
 
